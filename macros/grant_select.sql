2cd25b77e6f7bfdef8369c57bcb4ac0fabfd1580
nathanskelton <nathanskelton@hotmail.fr>
2026-05-04 16:00:12 +0200

    added clean_stale_models macro

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,8 +1,8 @@
 {% macro grant_select(schema=target.schema, role=target.role) %}
     {% set sql %}
-        grant usage on schema {{ schema }} to {{ role }};
-        grant select on all tables in schema {{ schema }} to {{ role }};
-        grant select on all views in schema {{ schema }} to {{ role }};
+        grant usage on schema {{ schema }} to role {{ role }};
+        grant select on all tables in schema {{ schema }} to role {{ role }};
+        grant select on all views in schema {{ schema }} to role {{ role }};
     {% endset %}
 
     {{ log('Granting select on all tables and views in schema ' ~ target.schema ~ ' to role ' ~ target.role, info=True) }}
